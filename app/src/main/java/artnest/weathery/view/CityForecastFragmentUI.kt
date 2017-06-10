02a95a17fa3b6af082303740f741fe7f7ccad83b
artnest <29A_10_apEwMeow>
2017-06-10 18:09:59 +0300

    Toolbar with shadow and default style reusable layout Forecast reusable root layout

--- app/src/main/java/artnest/weathery/view/CityForecastFragmentUI.kt
+++ app/src/main/java/artnest/weathery/view/CityForecastFragmentUI.kt
@@ -1,7 +1,6 @@
 package artnest.weathery.view
 
 import android.content.res.ColorStateList
-import android.os.Build
 import android.support.design.widget.FloatingActionButton
 import android.support.v4.content.ContextCompat
 import android.support.v7.widget.Toolbar
@@ -9,8 +8,8 @@ import android.view.Gravity
 import android.widget.TextView
 import artnest.weathery.R
 import artnest.weathery.controller.fragments.CityForecastFragment
+import artnest.weathery.utils.toolbarShadow
 import org.jetbrains.anko.*
-import org.jetbrains.anko.appcompat.v7.toolbar
 import org.jetbrains.anko.design.floatingActionButton
 
 class CityForecastFragmentUI : AnkoComponent<CityForecastFragment> {
@@ -20,45 +19,13 @@ class CityForecastFragmentUI : AnkoComponent<CityForecastFragment> {
     lateinit var tv: TextView
 
     override fun createView(ui: AnkoContext<CityForecastFragment>) = with(ui) {
-        relativeLayout {
-            // TODO extract toolbar_shadow -> include() into layout
-            tb = toolbar {
-                backgroundColor = ContextCompat.getColor(ctx, R.color.colorAccent)
-                setTitleTextColor(ContextCompat.getColor(ctx, android.R.color.white))
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                    elevation = 4 * ctx.resources.displayMetrics.density
-                }
-            }.lparams {
-                width = matchParent
-                height = wrapContent
-            }
-
-            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
-                view {
-                    background = ContextCompat.getDrawable(ctx, R.drawable.shadow)
-                }.lparams {
-                    width = matchParent
-                    height = dip(4)
-                    bottomOf(tb)
-                }
-            }
-
-            // TODO extract layout -> customize()
+        ForecastRootLayout<CityForecastFragment>({
             tv = textView {
                 text = "Test Weather"
                 gravity = Gravity.CENTER
             }.lparams {
                 centerInParent()
             }
-
-            fab = floatingActionButton {
-                imageResource = R.drawable.ic_globe
-                backgroundTintList = ColorStateList.valueOf(ContextCompat.getColor(ctx, R.color.colorPrimary))
-            }.lparams {
-                margin = dip(20)
-                alignParentBottom()
-                alignParentRight()
-            }
-        }
+        }).createView(ui)
     }
 }

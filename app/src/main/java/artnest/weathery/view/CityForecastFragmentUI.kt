243ff01b20f361e0fbda5248dac282e24ec969c4
artnest <29A_10_apEwMeow>
2017-06-10 18:48:22 +0300

    Moved forecast root layout back to fragment UI code (elevation not working)

--- app/src/main/java/artnest/weathery/view/CityForecastFragmentUI.kt
+++ app/src/main/java/artnest/weathery/view/CityForecastFragmentUI.kt
@@ -1,6 +1,7 @@
 package artnest.weathery.view
 
 import android.content.res.ColorStateList
+import android.os.Build
 import android.support.design.widget.FloatingActionButton
 import android.support.v4.content.ContextCompat
 import android.support.v7.widget.Toolbar
@@ -8,8 +9,8 @@ import android.view.Gravity
 import android.widget.TextView
 import artnest.weathery.R
 import artnest.weathery.controller.fragments.CityForecastFragment
-import artnest.weathery.utils.toolbarShadow
 import org.jetbrains.anko.*
+import org.jetbrains.anko.appcompat.v7.toolbar
 import org.jetbrains.anko.design.floatingActionButton
 
 class CityForecastFragmentUI : AnkoComponent<CityForecastFragment> {
@@ -19,13 +20,50 @@ class CityForecastFragmentUI : AnkoComponent<CityForecastFragment> {
     lateinit var tv: TextView
 
     override fun createView(ui: AnkoContext<CityForecastFragment>) = with(ui) {
-        ForecastRootLayout<CityForecastFragment>({
+        relativeLayout {
+            tb = toolbar {
+                id = R.id.toolbar
+                backgroundColor = ContextCompat.getColor(ctx, R.color.colorAccent)
+                setTitleTextColor(ContextCompat.getColor(ctx, android.R.color.white))
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    elevation = 4 * ctx.resources.displayMetrics.density
+                }
+            }.lparams {
+                width = matchParent
+
+                val styledAttributes = ctx.theme.obtainStyledAttributes(
+                        intArrayOf(android.R.attr.actionBarSize))
+                height = styledAttributes.getDimension(
+                        0, 56 * ctx.resources.displayMetrics.density).toInt()
+                styledAttributes.recycle()
+            }
+
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+                view {
+                    background = ContextCompat.getDrawable(ctx, R.drawable.shadow)
+                }.lparams {
+                    width = matchParent
+                    height = dip(4)
+                    bottomOf(tb)
+                }
+            }
+
             tv = textView {
                 text = "Test Weather"
                 gravity = Gravity.CENTER
             }.lparams {
                 centerInParent()
             }
-        }).createView(ui)
+
+            fab = floatingActionButton {
+                imageResource = R.drawable.ic_globe
+                backgroundTintList = ColorStateList.valueOf(
+                        ContextCompat.getColor(ctx, R.color.colorPrimary))
+            }.lparams {
+                margin = dip(20)
+                alignParentBottom()
+                alignParentRight()
+            }
+        }
     }
 }

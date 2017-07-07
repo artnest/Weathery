6eb4f727f61c1c7c9ca148d9e915afa72a9a203e
artnest <artem291997@gmail.com>
2017-07-07 14:17:08 +0300

    Added Google Maps Added MapView to map fragment page

--- app/src/main/java/artnest/weathery/controller/fragments/MapFragment.kt
+++ app/src/main/java/artnest/weathery/controller/fragments/MapFragment.kt
@@ -3,21 +3,68 @@ package artnest.weathery.controller.fragments
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
-import artnest.weathery.view.MapFragmentUI
-import org.jetbrains.anko.AnkoContext
-import org.jetbrains.anko.support.v4.ctx
+import artnest.weathery.R
+import artnest.weathery.helpers.inflate
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.MapsInitializer
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.map_fragment.view.*
+import org.jetbrains.anko.support.v4.act
 
 class MapFragment : Fragment() {
 
-    private lateinit var mapFragmentUI: MapFragmentUI
+    lateinit var mMapView: MapView
+    private lateinit var mMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        mapFragmentUI = MapFragmentUI()
     }
 
-    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?) =
-            mapFragmentUI.createView(AnkoContext.create(ctx, this))
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        val v = container!!.inflate(R.layout.map_fragment)
+        mMapView = v.mapView
+
+        mMapView.onCreate(savedInstanceState)
+        mMapView.onResume()
+
+        MapsInitializer.initialize(act.applicationContext)
+
+        mMapView.getMapAsync { googleMap ->
+            run {
+                mMap = googleMap
+                mMap.isMyLocationEnabled = true
+
+                val sydney = LatLng(-34.0, 151.0)
+                mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+            }
+        }
+
+        return v
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mMapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mMapView.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mMapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mMapView.onLowMemory()
+    }
 }
